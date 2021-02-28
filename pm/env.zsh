ef50388983c73e12336785c69ad829f1fe760f09
tianshi25 <tianshi25g@gmail.com>
2021-03-01 00:22:20 +0800

    update config

--- pm/env.zsh
+++ pm/env.zsh
@@ -11,4 +11,6 @@ if [[ ! -f ${HOME}/LocalConfig/pm ]] then
     mkdir -p ${HOME}/LocalConfig/pm
 fi
 
+# TODO sudo cp _pm /usr/local/share/zsh/site-functions/
+
 export PM_BASE=${HOME}/LocalConfig/pm
