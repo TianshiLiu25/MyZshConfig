387e7c956412eb31c36c56a2e2eba28c394a46c1
tianshi25 <tianshi25g@gmail.com>
2021-02-28 21:12:38 +0800

    update config

--- tmux/env.zsh
+++ tmux/env.zsh
@@ -1,3 +1,5 @@
 local DEFAULT_TMUX_CONFIG_PATH="${HOME}/.tmux.conf"
 
-ln -s $(dirname $0:A)/tmux.conf $DEFAULT_TMUX_CONFIG_PATH
+if [ ! -f $(dirname $0:A)/tmux.conf ]; then
+    ln -s $(dirname $0:A)/tmux.conf $DEFAULT_TMUX_CONFIG_PATH
+fi
