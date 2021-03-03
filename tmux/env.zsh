3882a6cd74ed49e01f25e4c006d3e2449c61d0ac
tianshi25 <tianshi25g@gmail.com>
2021-03-03 23:30:41 +0800

    update

--- tmux/env.zsh
+++ tmux/env.zsh
@@ -2,5 +2,5 @@ alias tmux="tmux -f $(dirname $0:A)/tmux.conf"
 
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     mkdir -p ~/.tmux/plugins/tpm
-    cp -r $(dirname $0:A)/tmux-plugins/* ~/.tmux/plugins/tpm
+    cp -r $(dirname $0:A)/tpm ~/.tmux/plugins/
 fi
