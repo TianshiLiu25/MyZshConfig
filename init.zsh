f2ab7c10b28c11acc62b9c4a3ba7e92ddba2fecc
tianshi25 <tianshi25g@gmail.com>
2021-03-04 19:56:29 +0800

    update

--- init.zsh
+++ init.zsh
@@ -1,5 +1,21 @@
-local DEFAULT_ZSH_CONFIG_PATH="${HOME}/.zshrc"
+# After git repo sync (git submodule update --init --remote)
+# run this script to init environment
 
-cp $DEFAULT_ZSH_CONFIG_PATH $DEFAULT_ZSH_CONFIG_PATH.backup
-rm -rf $DEFAULT_ZSH_CONFIG_PATH
+INIT_CONFIG_BACKUP="${HOME}/$(date +"%Y%m%d_%H%M%S")_InitConfigBackup"
+mkdir -p $INIT_CONFIG_BACKUP
+
+# zsh
+DEFAULT_ZSH_CONFIG_PATH="${HOME}/.zshrc"
+mv $DEFAULT_ZSH_CONFIG_PATH $INIT_CONFIG_BACKUP/zshrc
 echo "source $(dirname $0:A)/env.zsh" > $DEFAULT_ZSH_CONFIG_PATH
+
+# tmux
+mv ${HOME}/.tmux.conf $INIT_CONFIG_BACKUP/tmux.conf
+mv ${HOME}/.tmux $INIT_CONFIG_BACKUP/tmux
+mkdir -p ~/.tmux/plugins/tpm
+cp $(dirname $0:A)/tmux/tmux.conf "${HOME}/.tmux.conf"
+cp -r $(dirname $0:A)/tmux/tpm ~/.tmux/plugins/
+
+# ranger
+mv ${HOME}/.config/ranger $INIT_CONFIG_BACKUP/
+cp -r $(dirname $0:A)/ranger ~/.config/
--- tmux/env.zsh
+++
@@ -1,6 +0,0 @@
-alias tmux="tmux -f $(dirname $0:A)/tmux.conf"
-
-if [[ ! -d ~/.tmux/plugins/tpm ]]; then
-    mkdir -p ~/.tmux/plugins/tpm
-    cp -r $(dirname $0:A)/tpm ~/.tmux/plugins/
-fi
